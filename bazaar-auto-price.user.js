478980d336ff7d71593e304532c64b6e616bb6c6
Tristan Lorne Dobrowney <92229541+DobrowneyT@users.noreply.github.com>
2025-11-10 10:28:28 -0600

    Update API key placeholder in user script

--- bazaar-auto-price.user.js
+++ bazaar-auto-price.user.js
@@ -35,7 +35,7 @@
  *
  */
 
-const apikey = 'LIMITED_API_KEY'; // ← EDIT THIS LINE WITH YOUR API KEY
+const apikey = 'YOUR_LIMITED-ACCESS_API_KEY'; // ← EDIT THIS LINE WITH YOUR API KEY
 
 // Category-based selling multipliers (applied to market_price)
 const CATEGORY_MULTIPLIERS = {
